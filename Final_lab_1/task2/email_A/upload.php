f19d85fdcc58c465685ad4fa3bfa04465aaaf38f
kabirXcode <kibreakabir4@gmail.com>
2026-04-12 16:49:27 +0600

    email task A

---
+++ Final_lab_1/task2/email_A/upload.php
@@ -0,0 +1,12 @@
+<?php
+    if(isset($_POST['submit'])){
+
+        $email = $_POST['email'];
+
+        if($email == ""){
+            echo "null email!";
+        }else{
+            echo "Your email is: " . $email;
+        }
+    }
+?>
