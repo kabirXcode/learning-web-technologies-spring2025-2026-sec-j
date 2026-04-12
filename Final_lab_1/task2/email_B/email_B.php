41c3a13a84164f706a750bacfb7932948a1c255c
kabirXcode <kibreakabir4@gmail.com>
2026-04-12 17:06:32 +0600

    Email task b

---
+++ Final_lab_1/task2/email_B/email_B.php
@@ -0,0 +1,34 @@
+<?php
+    $email = "";
+
+    if(isset($_POST['submit'])){
+
+        $email = $_POST['email'];
+
+        if($email == ""){
+            echo "null email!";
+        }else{
+            echo "Your email is: " . $email;
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Email Input</title>
+</head>
+<body>
+
+    <form method="post" action="" enctype="multipart/form-data">
+        <fieldset style="width: 400px;">
+            <legend><b>EMAIL</b></legend>
+
+            Email: <input type="email" name="email" value=""/> <br><br>
+
+            <hr>
+            <input type="submit" name="submit" value="Submit"/>
+        </fieldset>
+    </form>
+
+</body>
+</html>
