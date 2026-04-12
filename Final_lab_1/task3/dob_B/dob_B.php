209ab4a91f5a9786e5bca558ec37fb1c3ec485f3
kabirXcode <kibreakabir4@gmail.com>
2026-04-12 17:28:34 +0600

    DOB B added

---
+++ Final_lab_1/task3/dob_B/dob_B.php
@@ -0,0 +1,46 @@
+<?php
+    $day = "";
+    $month = "";
+    $year = "";
+
+    if(isset($_POST['submit'])){
+
+        $day = $_POST['day'];
+        $month = $_POST['month'];
+        $year = $_POST['year'];
+
+        if($day == "" || $month == "" || $year == ""){
+            echo "null date of birth!";
+        }else{
+            echo "Date of Birth: " . $day . "/" . $month . "/" . $year;
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Date of Birth</title>
+</head>
+<body>
+
+    <form method="post" action="" enctype="multipart/form-data">
+        <fieldset style="width: 450px;">
+            <legend><b>DATE OF BIRTH</b></legend>
+
+            dd
+            <input type="text" name="day" value="" size="2"/> /
+
+            mm
+            <input type="text" name="month" value="" size="2"/> /
+
+            yyyy
+            <input type="text" name="year" value="" size="4"/> 
+            <br><br>
+
+            <hr>
+            <input type="submit" name="submit" value="Submit"/>
+        </fieldset>
+    </form>
+
+</body>
+</html>
