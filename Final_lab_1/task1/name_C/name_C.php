6f6b65730112b837a659ce7b2c647e3cc9bad218
kabirXcode <kibreakabir4@gmail.com>
2026-04-12 15:12:05 +0600

    Name Task C

---
+++ Final_lab_1/task1/name_C/name_C.php
@@ -0,0 +1,36 @@
+<?php
+    $name = "";
+
+    if(isset($_POST['submit'])){
+
+        $name = $_POST['name'];
+
+        if($name == ""){
+            echo "null name!";
+        }else{
+            echo "Your name is: " . $name;
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Name Input</title>
+</head>
+<body>
+
+    <form method="post" action="" enctype="multipart/form-data">
+        <fieldset style="width: 400px;">
+            <legend><b>NAME</b></legend>
+
+            Name: <input type="text" name="name" 
+            value="<?php echo $name; ?>"/> <br><br>
+
+            <hr>
+            <input type="submit" name="submit" value="Submit"/>
+        </fieldset>
+    </form>
+
+</body>
+</html>
